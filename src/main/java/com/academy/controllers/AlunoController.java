99a653cbebfc7eb3bc3c197c5b0767a404800cef
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-08-25 15:03:45 -0300

    funcionando a view e o metodo de edição de aluno

--- src/main/java/com/academy/controllers/AlunoController.java
+++ src/main/java/com/academy/controllers/AlunoController.java
@@ -1,8 +1,12 @@
 package com.academy.controllers;
 
+import java.util.Optional;
+import java.util.UUID; 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.academy.model.Aluno;
@@ -23,7 +27,9 @@ public class AlunoController {
         return mv;
     }
 
-    @PostMapping("/InsertAlunos")
+    
+
+    @PostMapping("/insertAlunos")
     public ModelAndView inserirAlunos(Aluno aluno) {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("redirect:/aluno/listAlunos");
@@ -31,13 +37,36 @@ public class AlunoController {
         return mv;
     }
 
+
     @GetMapping("/aluno/listAlunos")
     public ModelAndView listgemAlunos() {
         ModelAndView mv = new ModelAndView();
-        mv.setViewName("aluno/listAlunos");
+        mv.setViewName("aluno/listAlunos"); // Corresponde ao arquivo listAlunos.html
         mv.addObject("alunosList", alunoRepository.findAll());
         return mv;
     }
+
+    @GetMapping("/alterar/{id}")
+    public ModelAndView alterar(@PathVariable("id") UUID id) {
+        ModelAndView mv = new ModelAndView();
+        Optional<Aluno> alunoOptional = alunoRepository.findById(id);
+        if (alunoOptional.isPresent()) {
+            mv.setViewName("aluno/alterarAluno");
+            mv.addObject("aluno", alunoOptional.get());
+        } else {
+            mv.setViewName("redirect:aluno/listAlunos"); // Redireciona se o aluno não for encontrado
+        }
+        return mv;
+    }
+
+    @PostMapping("/alterar")
+    public ModelAndView alterar(Aluno aluno) {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("redirect:/aluno/listAlunos");
+        alunoRepository.save(aluno);
+        return mv;
+    }
+
 
     
         
