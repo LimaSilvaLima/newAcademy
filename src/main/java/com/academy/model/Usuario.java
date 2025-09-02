e6d55b19fe3a9511da8429909f247df7fa353404
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-09-01 21:13:54 -0300

    concluida e estilizada a tela de login

--- src/main/java/com/academy/model/Usuario.java
+++ src/main/java/com/academy/model/Usuario.java
@@ -6,6 +6,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Entity
+
 public class Usuario {
     
     @Id
