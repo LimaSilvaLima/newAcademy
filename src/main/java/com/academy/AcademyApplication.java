b79fd009acbd179fd467d164e213c73eb5bdb6fe
LimaSilvaLima <147210553+LimaSilvaLima@users.noreply.github.com>
2025-09-13 20:47:56 -0300

    implantada configuraçao de ambiente com DotEnv

--- src/main/java/com/academy/AcademyApplication.java
+++ src/main/java/com/academy/AcademyApplication.java
@@ -2,13 +2,16 @@ package com.academy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class AcademyApplication {
 
 	
 	public static void main(String[] args) {
+		Dotenv.configure().systemProperties().load();
 		SpringApplication.run(AcademyApplication.class, args);
+		
 	}
 
 
